cf85e1df98d77ccc0d8457367e339ecf45e6fa96
Nikita Chupahin <nik123chupahin@gmail.com>
2025-08-19 20:49:21 +0300

    make build

---
+++ index.5dcae22f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),o=e.offsetWidth,f=e.offsetHeight,c=t.offsetWidth,l=t.offsetHeight;t.style.top="".concat(f/2-l/2-10,"px"),t.style.left="".concat(o/2-c/2-10,"px");
+//# sourceMappingURL=index.5dcae22f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+const topValue = wallHeight / 2 - spiderHeight / 2;
+const leftValue = wallWidth / 2 - spiderWidth / 2;
+spider.style.top = `${topValue - 10}px`;
+spider.style.left = `${leftValue - 10}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
